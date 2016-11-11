1df4b77beffad1b490290820dae4b89ea3839fad
Maximilian Kiesner <maximilian.kiesner@stud-mail.uni-wuerzburg.de>
2016-11-11 05:25:01 +0100

    rational number class with overloaded operator* as non-member function -- implicit type conversion and thus multiplication with other numbers possible

---
+++ rational/rational.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class rational
+{
+	public:
+		rational(int num=0, int denom=1) : num(num), denom(denom) {}
+
+		int numerator() const { return num; }
+		int denominator() const { return denom;}
+		//friend std::ostream& operator<<(std::ostream& os, const 
+	private:
+		int num;
+		int denom;	
+};
+
+std::ostream& operator<<(std::ostream& os, const rational& r)
+{
+	return os << r.numerator() << "/" << r.denominator();
+}
+const rational operator*(const rational& a, const rational& b)
+{
+	return rational(a.numerator() * b.numerator(), a.denominator() * b.denominator());
+}
+
+int main()
+{
+	rational a(1, 3);
+	rational b(3, 4);
+
+	int x = 5;
+
+	std::cout << x*a << std::endl;
+
+	return 0;
+}
