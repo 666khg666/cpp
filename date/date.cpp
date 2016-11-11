7d806c446d50b1e43877e6723530a7192585b5cd
Maximilian Kiesner <maximilian.kiesner@stud-mail.uni-wuerzburg.de>
2016-11-11 02:31:36 +0100

    Encapsulate month as class

--- date/date.cpp
+++ date/date.cpp
@@ -11,11 +11,26 @@ struct day
 	int val;
 };
 
-struct month
+class month
 {
-	explicit month(int v) : val(v) {}
+	public:
+		static month january() { return month(1); }
+		static month february() { return month(2); }
+		static month march() { return month(3); }
+		static month april() { return month(4); }
+		static month may() { return month(5); }
+		static month june() { return month(6); }
+		static month july() { return month(7); }
+		static month august() { return month(8); }
+		static month september() { return month(9); }
+		static month october() { return month(10); }
+		static month november() { return month(11); }
+		static month december() { return month(12); }
 
-	int val;
+		int get_val() const { return val; }
+	private:
+		explicit month(int v) : val(v) {}
+		int val;
 };
 
 struct year
@@ -28,7 +43,7 @@ struct year
 class date
 {
 	public:
-		explicit date(const struct day& d, const struct month& m, const struct year& y) : day(d.val), month(m.val), year(y.val) {}
+		explicit date(const struct day& d, const month& m, const struct year& y) : day(d.val), month(m.get_val()), year(y.val) {}
 
 		friend std::ostream& operator<<(std::ostream& os, const date& d);
 	private:
@@ -44,7 +59,7 @@ std::ostream& operator<<(std::ostream& os, const date& d)
 
 int main()
 {
-	date now(day(11), month(11), year(2016));
+	date now(day(11), month::november(), year(2016));
 
 	std::cout << now << std::endl;
 	return 0;
